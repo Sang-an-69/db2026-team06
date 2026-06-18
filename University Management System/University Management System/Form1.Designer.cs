03098af6c0f9199e1e2811e7d80e3c78e095a3d3
Changge Sang (25101324) <s25101324@stu.edu.tw>
2026-06-18 09:49:29 +0800

    Add files via upload

---
+++ University Management System/University Management System/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace University_Management_System
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.loginbutton = new System.Windows.Forms.Button();
+            this.usernameTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.loginbutton);
+            this.groupBox1.Controls.Add(this.passwordTextbox);
+            this.groupBox1.Controls.Add(this.usernameTextbox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 133);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.Location = new System.Drawing.Point(79, 86);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(100, 23);
+            this.loginbutton.TabIndex = 1;
+            this.loginbutton.Text = "Login";
+            this.loginbutton.UseVisualStyleBackColor = true;
+            this.loginbutton.Click += new System.EventHandler(this.loginbutton_Click);
+            // 
+            // usernameTextbox
+            // 
+            this.usernameTextbox.Location = new System.Drawing.Point(79, 24);
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(100, 25);
+            this.usernameTextbox.TabIndex = 2;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(79, 55);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.PasswordChar = '*';
+            this.passwordTextbox.Size = new System.Drawing.Size(100, 25);
+            this.passwordTextbox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 301);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("PMingLiU", 11F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loginbutton;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.TextBox usernameTextbox;
+    }
+}
+
---
+++ University Management System/University Management System/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace University_Management_System
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginbutton_Click(object sender, EventArgs e)
+        {
+            // validate input
+            if (string.IsNullOrWhiteSpace(usernameTextbox.Text) ||
+            string.IsNullOrWhiteSpace(passwordTextbox.Text))
+            {
+                MessageBox.Show("username and password is required!", 
+                                "Error", 
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+            
+            // open database
+            using (var db = new UniversityEntities())
+            {
+                
+                // check if user exists
+                var user = db.users.Where(u => u.username == usernameTextbox.Text 
+                && u.password == passwordTextbox.Text).FirstOrDefault();
+                if (user == null)
+                {
+                    MessageBox.Show("invalid username or password!", 
+                                    "Error", 
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                    return;
+                }
+                
+                // hide login form
+                usernameTextbox.Text = "";
+                passwordTextbox.Text = "";
+                this.Hide();
+
+                // open form based on role
+                switch (user.user_type)
+                {
+                    
+                        // administrator
+                    case "administrator":
+                        var adminForm = new AdminForm();
+                        adminForm.ShowDialog();
+                        break;
+                    
+                        // instructor
+                    case "instructor":
+                        var facultyForm = new InstructorForm();
+                        facultyForm.ShowDialog();
+                        break;
+
+                    
+                        // student
+                    case "student":
+                        var studentForm = new StudentForm();
+                        studentForm.ShowDialog();
+                        break;
+                }
+                
+                // show login form
+                this.Show();
+            }
+        }
+    }
+}
